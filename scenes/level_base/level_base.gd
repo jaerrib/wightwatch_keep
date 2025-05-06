513d78407d227e2f2b0161932be3197cb6084736
jaerrib <jaerrib@tutanota.com>
2025-05-06 19:00:58 -0400

    Set color overlay directly instead of exporting variable

--- scenes/level_base/level_base.gd
+++ scenes/level_base/level_base.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-@export var color_overlay: String = "00000080"
-
 @onready var music: AudioStreamPlayer = $Music
 @onready var level_overlay: ColorRect = $CanvasLayer/LevelOverlay
 
 
 func _ready() -> void:
-	level_overlay.color = Color(color_overlay)
+	pass
 
 
 func _process(_delta: float) -> void:
