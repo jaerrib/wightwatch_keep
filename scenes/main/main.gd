dcbebd503fae7d8aa1c72ec67fb4cda0085e478c
jaerrib <jaerrib@tutanota.com>
2025-05-11 10:34:49 -0400

    Separate intro sequence function

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -10,6 +10,7 @@ const WIND = "wind"
 var _intro_playing: bool = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var flicker_timer: Timer = $FlickerTimer
 @onready var music: AudioStreamPlayer = $Music
 @onready var pulse_delay: Timer = $PulseDelay
 
@@ -23,13 +24,18 @@ func _process(_delta: float) -> void:
 	if !music.has_stream_playback():
 		start_game()
 	if Input.is_action_just_pressed("jump") and !_intro_playing:
-		music.stop()
-		music.stream = SOUNDS[INTRO]
-		_intro_playing = true
-		music.play()
-		pulse_delay.start()
-		
-		
+		start_intro()
+
+
+func start_intro() -> void:
+	music.stop()
+	music.stream = SOUNDS[INTRO]
+	_intro_playing = true
+	music.play()
+	flicker_timer.stop()
+	pulse_delay.start()
+
+
 func start_game() -> void:
 	PlayerManager.set_initial_stats()
 	GameManager.load_next_level_scene()
