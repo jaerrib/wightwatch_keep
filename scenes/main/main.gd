0254dece15b895e32e2b9b79c4f3a6b0c8325e58
jaerrib <jaerrib@tutanota.com>
2025-05-11 10:11:14 -0400

    Add flicker animation, readd overlay

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,16 +1,22 @@
 extends Control
 
 const INTRO = "intro"
-
-var SOUNDS: Dictionary = {
-	INTRO: preload("res://assets/audio/Intro.ogg")
+const SOUNDS: Dictionary = {
+	INTRO: preload("res://assets/audio/Intro.ogg"),
+	WIND: preload("res://assets/audio/wind.ogg")
 }
+const WIND = "wind"
 
 var _intro_playing: bool = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var music: AudioStreamPlayer = $Music
 @onready var pulse_delay: Timer = $PulseDelay
+
+
+func _ready() -> void:
+	music.stream = SOUNDS[WIND]
+	music.play()
 
 
 func _process(_delta: float) -> void:
@@ -31,3 +37,7 @@ func start_game() -> void:
 
 func _on_pulse_delay_timeout() -> void:
 	animation_player.play("pulse")
+
+
+func _on_flicker_timer_timeout() -> void:
+	animation_player.play("flicker")
