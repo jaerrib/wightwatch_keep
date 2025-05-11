9140bea909cd652e97eb08edd90de86af0e48245
jaerrib <jaerrib@tutanota.com>
2025-05-11 09:24:15 -0400

    Add wind, intro music, and text pulse to title screen

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,10 +1,33 @@
 extends Control
 
+const INTRO = "intro"
 
-func _ready() -> void:
-	pass
+var SOUNDS: Dictionary = {
+	INTRO: preload("res://assets/audio/Intro.ogg")
+}
+
+var _intro_playing: bool = false
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var music: AudioStreamPlayer = $Music
+@onready var pulse_delay: Timer = $PulseDelay
+
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("jump"):
-		PlayerManager.set_initial_stats()
-		GameManager.load_next_level_scene()
+	if !music.has_stream_playback():
+		start_game()
+	if Input.is_action_just_pressed("jump") and !_intro_playing:
+		music.stop()
+		music.stream = SOUNDS[INTRO]
+		_intro_playing = true
+		music.play()
+		pulse_delay.start()
+		
+		
+func start_game() -> void:
+	PlayerManager.set_initial_stats()
+	GameManager.load_next_level_scene()
+
+
+func _on_pulse_delay_timeout() -> void:
+	animation_player.play("pulse")
