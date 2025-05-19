af5a1338069d742140d859573df82345e36466b2
jaerrib <jaerrib@tutanota.com>
2025-05-19 12:46:40 -0400

    Add additional floor detection to ensure flask queues free if it rotates in flight

--- scenes/flask/flask.gd
+++ scenes/flask/flask.gd
@@ -3,6 +3,9 @@ extends RigidBody2D
 @export var sprite: Sprite2D
 
 @onready var floor_detection: RayCast2D = $FloorDetection
+@onready var floor_detection_2: RayCast2D = $FloorDetection2
+@onready var floor_detection_3: RayCast2D = $FloorDetection3
+@onready var floor_detection_4: RayCast2D = $FloorDetection4
 
 
 func setup(direction: Vector2, speed: float, flip_sprite: bool) -> void:
@@ -11,7 +14,12 @@ func setup(direction: Vector2, speed: float, flip_sprite: bool) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if floor_detection.is_colliding():
+	if (
+		floor_detection.is_colliding() or
+		floor_detection_2.is_colliding() or
+		floor_detection_3.is_colliding() or
+		floor_detection_4.is_colliding()
+		):
 		queue_free()
 
 
