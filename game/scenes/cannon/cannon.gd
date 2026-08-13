044d259ca855dae9c82ef4cbfd362205a9efa60b
jaerrib <jaerrib@tutanota.com>
2026-08-13 05:31:23 -0400

    Stop cannon fire on exit reached

--- game/scenes/cannon/cannon.gd
+++ game/scenes/cannon/cannon.gd
@@ -2,11 +2,13 @@ extends StaticBody2D
 
 @export var flipped: bool = false
 
+var _can_fire: bool = true
 var _direction: Vector2 = Vector2.ZERO
 var _shoot_position: Vector2
 var _speed: float = 50
 var _flip_sprite: bool = false
 
+@onready var shoot_timer: Timer = $ShootTimer
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
@@ -18,9 +20,12 @@ func _ready() -> void:
 		_direction = Vector2.LEFT
 	_flip_sprite = sprite_2d.flip_h
 	set_shoot_position()
+	SignalManager.on_exit_reached.connect(on_exit_reached)
 
 
 func _on_shoot_timer_timeout() -> void:
+	if not _can_fire:
+		return
 	SignalManager.on_create_object.emit(_shoot_position, Constants.ObjectType.EXPLOSION)
 	SignalManager.on_create_projectile.emit(
 		_shoot_position,
@@ -37,3 +42,8 @@ func set_shoot_position():
 	else:
 		_shoot_position.x = global_position.x - 12
 	_shoot_position.y = global_position.y - 3
+
+
+func on_exit_reached() -> void:
+	_can_fire = false
+	shoot_timer.stop()
