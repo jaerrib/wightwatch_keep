a501edcec8bb5ba6fdc61451b7d19d62110c122b
jaerrib <jaerrib@tutanota.com>
2026-09-05 16:05:20 -0400

    Add tutorial screen at launch

---
+++ game/scenes/tutorial_page/tutorial_page.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("jump"):
+		GameManager.load_main_scene()
